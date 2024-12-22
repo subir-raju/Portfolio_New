9afcfc8972501bc6ffff4ca32ec64c0d2dc210ef
subir-raju <subir.raju0@gmail.com>
2024-12-22 22:51:01 +0200

    About Section

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import './About.css'
+import profile from '../../assets/profile.png'
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="about-title">
+        <h1>About me</h1>
+      </div>
+      <div className="about-section">
+        <div className="about-left">
+          <img src={profile} alt=" " />
+        </div>
+        <div className="about-right">
+          <div className="about-para">
+            <p>
+              I am Subir Dey, a current masters degree student in data science
+              in Tampere University.
+            </p>
+          </div>
+          <div className="about-skills">
+            <div className="about-skill">
+              <p>HTML & CSS</p>
+              <hr style={{ width: "50%" }} />
+            </div>
+            <div className="about-skill">
+              <p>React JS</p>
+              <hr style={{ width: "90%" }} />
+            </div>
+            <div className="about-skill">
+              <p>JavaScript</p>
+              <hr style={{ width: "70%" }} />
+            </div>
+            <div className="about-skill">
+              <p>Python</p>
+              <hr style={{ width: "70%" }} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="about-achievements">
+        <div className="about-achievement">
+          <h1>10+</h1>
+          <p>YEARS OF EXPERIENCE</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>90+</h1>
+          <p>PROJECTS COMPLETED</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>15+</h1>
+          <p>HAPPY CLIENTS</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About
