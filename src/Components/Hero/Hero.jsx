12b8d5482687e0ca22cd074d454a7e1929685b8a
subir-raju <subir.raju0@gmail.com>
2024-12-22 20:26:00 +0200

    my site

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Hero.css'
+import profile_img from '../../assets/propic.png'
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <img src={profile_img} alt=" " />
+      <h1>
+        <span>I'm Subir Dey,</span> frontend developer based in Finland
+      </h1>
+      <p>
+        I am a frontend developer from Tampere, Finland with 4 years of
+        Experience
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-resume">Resume</div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero
