57a1c3576ac040a94d9f7e97e89507f45423aebd
subir-raju <subir.raju0@gmail.com>
2024-12-23 01:39:35 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
 import Project from './Components/Project/Project'
 import Contact from './Components/Contact/Contact'
+import { Footer } from './Components/Footer/Footer'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <About />
       <Project />
       <Contact />
+      <Footer />
     </div>
   )
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+export const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className="footer-bottom">
+            <p className="footer-bottom-left">© 2024 Subir</p>
+        </div>
+    </div>
+  )
+}
