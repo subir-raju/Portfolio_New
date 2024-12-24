025cfd1bdc3ffcb24d37b41dd8b50acf0b45c738
subir-raju <subir.raju0@gmail.com>
2024-12-24 22:10:06 +0200

    footer misplaced

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from "react";
+import './Blogs.css'
 
-const Blogs = () => {
+const Blog = () => {
+  const [activeBlog, setActiveBlog] = useState(null);
+
+  const blogData = [
+    {
+      title: "Blog Post 1",
+      details: <p>This is the content of the first blog post.</p>,
+    },
+    {
+      title: "Blog Post 2",
+      details: <p>This is the content of the second blog post.</p>,
+    },
+  ];
+
+  const handleToggleDetails = (index) => {
+    if (activeBlog === index) {
+      setActiveBlog(null);
+    } else {
+      setActiveBlog(index);
+    }
+  };
+
   return (
-    <div>Blogs</div>
-  )
-}
+    <div className="blog-page">
+      <h1>My Blog</h1>
+      {blogData.length === 0 ? (
+        <div className="no-posts">
+          <p>Ooopsi, the author is too lazy to write something here!!</p>
+        </div>
+      ) : (
+        <div className="blog-list">
+          {blogData.map((blog, index) => (
+            <div className="blog-item" key={index}>
+              <div
+                className={`blog-title ${activeBlog === index ? "active" : ""}`}
+                onClick={() => handleToggleDetails(index)}
+              >
+                {blog.title}
+              </div>
+              {activeBlog === index && (
+                <div className="blog-details">{blog.details}</div>
+              )}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default Blogs
+export default Blog;
