4563f61bb651b65cae088372b98c0072697f3f4c
subir-raju <subir.raju0@gmail.com>
2024-12-24 22:18:39 +0200

    one more edit

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -58,22 +58,6 @@ const About = () => {
           </div>
         </div>
       </div>
-      <div className="about-achievements">
-        <div className="about-achievement">
-          <h1>10+</h1>
-          <p>YEARS OF EXPERIENCE</p>
-        </div>
-        <hr />
-        <div className="about-achievement">
-          <h1>90+</h1>
-          <p>PROJECTS COMPLETED</p>
-        </div>
-        <hr />
-        <div className="about-achievement">
-          <h1>15+</h1>
-          <p>HAPPY CLIENTS</p>
-        </div>
-      </div>
     </div>
   );
 };
